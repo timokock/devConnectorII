1e7dac9a3ad7d9c4566b12984d02fd3072d4ea9e
Timo Kock <timokock@gmail.com>
2019-11-30 21:56:35 +0100

    Register & Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import { loadUser } from './actions/auth';
+import setAuthToken from "./utils/setAuthToken";
 
 // Redux
 import { Provider } from "react-redux";
@@ -12,22 +14,33 @@ import store from "./store";
 
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+};
+
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+}
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from "react";
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     
     const [formData, setFormData] = useState({
         name: '',
@@ -25,9 +26,14 @@ const Register = ({ setAlert }) => {
         if(password !== password2){
             setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('SUCCESS');   
+            register({ name, email, password });
         }
     };
+
+  // Redirect user when register success
+  if(isAuthenticated) {
+    return <Redirect to="/dashboard" />
+  }
 
 
   return (
@@ -38,20 +44,20 @@ const Register = ({ setAlert }) => {
       </p>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" name="password" minLength="6" value={password} onChange={e => onChange(e)} required />
+          <input type="password" placeholder="Password" name="password" minLength="6" value={password} onChange={e => onChange(e)} />
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Confirm Password" name="password2" minLength="6" value={password2} onChange={e => onChange(e)} required />
+          <input type="password" placeholder="Confirm Password" name="password2" minLength="6" value={password2} onChange={e => onChange(e)} />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
@@ -63,7 +69,13 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
-}
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
